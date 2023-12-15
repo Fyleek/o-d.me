f108f38a5363e68243652fe5886314050a2082ac
Decloquement Oscar <odecloquement@gmail.com>
2023-12-15 01:58:12 +0100

    Manage language

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -3,10 +3,11 @@ import { Listbox } from "@headlessui/react";
 import clsx from "clsx";
 import { motion, AnimatePresence } from "framer-motion";
 import ReactCountryFlag from "react-country-flag";
+import { useLang } from "./LanguageProvider";
 
 export default function LanguageSwitcher() {
   const [mounted, setMounted] = useState(false);
-  const [language, setLanguage] = useState("FR"); // Default value
+  const { lang, setLang } = useLang();
 
   useEffect(() => {
     setMounted(true);
@@ -18,29 +19,28 @@ export default function LanguageSwitcher() {
 
   const languages = [
     { code: "EN", name: "English", countryCode: "US" },
-    { code: "FR", name: "Français", countryCode: "FR" }
+    { code: "FR", name: "Français", countryCode: "FR" },
   ];
 
   const flagStyle: CSSProperties = {
-    width: '1.5em',
-    height: '1.5em',
-    borderRadius: '50%',
-    objectFit: 'cover',
-    overflow: 'hidden',
-    border: '1px solid #ddd',
-    boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)'
+    width: "1.5em",
+    height: "1.5em",
+    borderRadius: "50%",
+    objectFit: "cover",
+    overflow: "hidden",
+    border: "1px solid #ddd",
+    boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.2)",
   };
 
-  const currentLanguage = languages.find(lang => lang.code === language) || languages[0];
+  const currentLanguage =
+    languages.find((langItem) => langItem.code === lang) || languages[0];
 
   return (
     <>
-      <Listbox value={language} onChange={setLanguage}>
+      <Listbox value={lang} onChange={(newLang) => setLang(newLang)}>
         {({ open }) => (
           <div className="relative">
-            <Listbox.Button
-              className="relative w-8 h-8 cursor-default rounded-full flex items-center justify-center focus:outline-none"
-            >
+            <Listbox.Button className="relative w-8 h-8 cursor-default rounded-full flex items-center justify-center focus:outline-none">
               <ReactCountryFlag
                 countryCode={currentLanguage.countryCode}
                 svg
@@ -57,31 +57,36 @@ export default function LanguageSwitcher() {
                   transition={{ type: "spring", bounce: 0.3, duration: 0.3 }}
                   className="absolute right-0 p-2 mt-2 overflow-auto text-base origin-top-right shadow-lg max-h-60 w-42 rounded-xl bg-white dark:bg-black focus:outline-none sm:text-sm capitalize"
                 >
-                  {languages.map((lang) => (
+                  {languages.map((langItem) => (
                     <Listbox.Option
-                      key={lang.code}
+                      key={langItem.code}
                       className={({ active }) =>
-                        clsx("relative cursor-default select-none py-2 pl-10 pr-4 rounded-md", active ? "bg-tertiary" : "")
+                        clsx(
+                          "relative cursor-default select-none py-2 pl-10 pr-4 rounded-md",
+                          active ? "bg-tertiary" : ""
+                        )
                       }
-                      value={lang.code}
+                      value={langItem.code}
                     >
                       {({ selected }) => (
                         <>
-                          <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
-                            {lang.name}
+                          <span
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
+                            }`}
+                          >
+                            {langItem.name}
                           </span>
-                          {(
-                            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                              <ReactCountryFlag
-                                countryCode={lang.countryCode}
-                                svg
-                                style={{
-                                  width: '1em',
-                                  height: '1em',
-                                }}
-                              />
-                            </span>
-                          )}
+                          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <ReactCountryFlag
+                              countryCode={langItem.countryCode}
+                              svg
+                              style={{
+                                width: "1em",
+                                height: "1em",
+                              }}
+                            />
+                          </span>
                         </>
                       )}
                     </Listbox.Option>
