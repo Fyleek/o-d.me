dcbc11358bc140bf9c2a52f7c33dbf1463773823
Decloquement Oscar <odecloquement@gmail.com>
2023-12-17 17:46:36 +0100

    Add views translations and adapt slug without language

--- translations/blogSlugTranslations.ts
+++ translations/blogSlugTranslations.ts
@@ -4,9 +4,11 @@ export const blogSlugTranslations : Translations = {
     EN: {
         loading: "Loading...",
         allBlog: "All Blogs",
+        views: "views",
     },
     FR: {
         loading: "Chargement...",
         allBlog: "Tous les Blogs",
+        views: "vues",
     }
 };
